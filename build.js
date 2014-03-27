dbdc62d051779334dbbcefe7746280038cbb947b
Malachi Griffie <malachi@nexussays.com>
2014-03-27 02:11:16 -0700

    Updated build script to actually compile instead of using r.js from CLI

--- build.js
+++ build.js
@@ -1,5 +1,6 @@
-//r.js.cmd -o build.js optimize=none
-({
+var requirejs = require('./node_modules/requirejs/bin/r.js');
+
+var defaultConfig = {
    baseUrl: "src",
    out: "./bin/nnet.js",
    //appDir: "./src",
@@ -7,5 +8,37 @@
    exclude: [],
    mainConfigFile: "./src/main.js",
    name: "main",
-   generateSourceMaps: true
-})
+   generateSourceMaps: true,
+   optimize: "none"
+};
+
+function extend(using)
+{
+   for(var prop in defaultConfig)
+   {
+      if(!(using.hasOwnProperty(prop)))
+      {
+         using[prop] = defaultConfig[prop];
+      }
+   }
+   return using;
+}
+
+//r.js.cmd -o build.js optimize=none
+requirejs.optimize(defaultConfig, console.log, console.err);
+
+//r.js.cmd -o build.js optimize=none paths.requireLib=../node_modules/requirejs/require include=requireLib
+requirejs.optimize(extend({
+   out: "./bin/nnet-with-require.js",
+   paths: {
+      "requireLib": "../node_modules/requirejs/require"
+   },
+   include: ["requireLib"]
+}), console.log, console.err);
+
+// build debug js
+requirejs.optimize(extend({
+   out: "./bin/debug.js",
+   mainConfigFile: "./src/nnet/debug/debug.js",
+   name: "nnet/debug/debug"
+}), console.log, console.err);
