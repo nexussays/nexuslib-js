2f7d1b41a26ad981740168266cc297a738df48b9
Malachi Griffie <malachi@nexussays.com>
2014-04-02 03:43:51 -0700

    Added Color class from nexuslib-as3; updated module indexes

--- src/nnet/core.js
+++ src/nnet/core.js
@@ -5,21 +5,6 @@ Math.randomInt = function(low, high)
    var L = parseInt(low, 10) || 0, H = parseInt(high, 10) || 1;
    return Math.floor( (Math.random() * (H - L + 1)) + L );
 };
-Math.mean = function()
-{
-   for(var mean = 0, ln = arguments.length, x = 0; x < ln; ++x)
-   {
-      mean += arguments[x];
-   }
-   return mean/ln;
-};
-Number.prototype.asUnsigned = function()
-{
-   //1. shift 0 in from right to set sign bit to 0
-   //2. multiply by two to retain sign bit (left shift would undo the previous right shift)
-   //3. restore the original 1's bit that was lost in the right shift
-   return ((this >>> 1) * 2) + (this & 0x1);
-};
 Number.prototype.days = function()
 {
    return this * (24).hours();
