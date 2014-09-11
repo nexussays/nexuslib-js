eada575b66bc072c8eaed5820056ed07f40bc728
Malachi Griffie <malachi@nexussays.com>
2014-09-11 14:48:26 -0700

    Updated build script

--- gruntfile.js
+++ gruntfile.js
@@ -5,6 +5,37 @@ module.exports = function(grunt)
    grunt.loadNpmTasks( 'grunt-contrib-uglify' );
    grunt.loadNpmTasks( 'grunt-contrib-copy' );
 
+   grunt.registerTask( "default", ["ts:build"] );
+
+   grunt.registerMultiTask( "gen", function()
+   {
+      generateModuleRoots(
+         this.data.template,
+         this.data.root,
+         this.data.regex,
+         this.data.ext,
+         this.data.extArr
+      );
+   } );
+
+   grunt.registerMultiTask( "def", function()
+   {
+      var dts = require( 'dts-bundle' );
+      dts.bundle( this.data );
+      var path = require( "path" );
+      var file = path.resolve( this.data.main, "../", this.data.out );
+      _fs.writeFileSync( file, _fs.readFileSync( file ).toString().replace( /__nnet\//g, "" ), 'utf-8' );
+   } );
+
+   grunt.registerMultiTask( "requirejs", function()
+   {
+      var r = require( "requirejs" );
+      //console.log("Running require.js for " + this.target);
+      r.optimize( this.data, console.out, console.err );
+   } );
+
+   grunt.registerTask( "optimize", ["gen:js-index", "def", "requirejs"] );
+
    var config =
    {
       paths:
@@ -26,7 +57,14 @@ module.exports = function(grunt)
             minified: "<%= paths.dest.root %>/bundled"
          }
       },
-      package: grunt.file.readJSON( './package.json' )
+      package: grunt.file.readJSON( './package.json' ),
+      copyDefaults: function()
+      {
+         for(var taskName in config)
+         {
+            copyDefaults( config[taskName] );
+         }
+      }
    };
 
    config.ts = {
@@ -35,12 +73,13 @@ module.exports = function(grunt)
          module: 'amd',
          sourceMap: true,
          declaration: true,
-         removeComments: true
+         removeComments: true,
+         default: {
+            src: ["<%= paths.src.main %>/**/*.ts", "<%= paths.src.test %>/*.ts"],
+            outDir: config.paths.dest.compiled
+         }
       },
-      default: {
-         src: ["<%= paths.src.main %>/**/*.ts", "<%= paths.src.test %>/*.ts"],
-         outDir: config.paths.dest.compiled
-      },
+      build: {},
       watch: {
          watch: "src"
       },
@@ -128,42 +167,43 @@ module.exports = function(grunt)
    // define after setting config initially so we can use existing config values
    config.requirejs =
    {
-      out: console.out,
-      err: console.err,
-      //r.js.cmd -o build.js optimize=none
-      main:
+      options:
       {
-         out: config.paths.dest.bundled + "/nnet-amd.js",
-         //appDir: config.paths.dest.compiled,
-         baseUrl: config.paths.dest.compiled,
-         //dir: "./bin",
-         //exclude: [],
-         //mainConfigFile: config.paths.dest.compiled + "/nnet_.js",
-         name: "_nnet",
-         generateSourceMaps: false,
-         optimize: "none"
-      },
-      builds:
-      [
-         // just use default config above
-         {},
-         //r.js.cmd -o build.js optimize=none paths.requireLib=../node_modules/requirejs/require include=requireLib
+         //r.js.cmd -o build.js optimize=none
+         default:
          {
-            out: config.paths.dest.bundled + "/nnet-amd-with-almond.js",
-            include: ["../../../lib/almond"],
-            //TODO: turn this back on once everything is properly wrapped
-            //wrap: true
-         },
-         //*
-         {
-            out: config.paths.dest.bundled + "/nnet-amd-with-require.js",
-            paths:
-            {
-               "requireLib": "../../../../node_modules/requirejs/require"
-            },
-            include: ["requireLib"]
+            out: config.paths.dest.bundled + "/nnet-amd.js",
+            //appDir: config.paths.dest.compiledMain,
+            baseUrl: config.paths.dest.compiledMain,
+            //dir: "./bin",
+            //exclude: [],
+            //mainConfigFile: config.paths.dest.compiledMain + "/nnet_.js",
+            name: "_nnet",
+            generateSourceMaps: false,
+            optimize: "none"
          }
-      ]
+      },
+      // just use default config above
+      main: {},
+      //r.js.cmd -o build.js optimize=none paths.requireLib=../node_modules/requirejs/require include=requireLib
+      almond:
+      {
+         out: config.paths.dest.bundled + "/nnet-amd-with-almond.js",
+         include: ["../../../lib/almond"],
+         //TODO: turn this back on once everything is properly wrapped
+         //wrap: true
+      },
+      //*
+      require:
+      {
+         out: config.paths.dest.bundled + "/nnet-amd-with-require.js",
+         paths:
+         {
+            "requireLib": "../../../../node_modules/requirejs/require"
+         },
+         include: ["requireLib"]
+      }
+
    };
 
    config.def = {
@@ -176,76 +216,46 @@ module.exports = function(grunt)
       }
    };
 
-   // copy settings from main to all other tasks unless their override
-   for(var targetName in config.ts)
+   function copyDefaults( taskObj )
    {
-      var target = config.ts[targetName];
-      for(var prop in config.ts.default)
+      if( taskObj && taskObj.options && taskObj.options.default )
       {
-         if(!target.hasOwnProperty( prop ))
+         // copy settings from main to all other tasks unless their override
+         for( var targetName in taskObj )
          {
-            target[prop] = config.ts.default[prop];
-         }
-         /* else if(prop == "options")
-         {
-            var options = config.ts.default[prop];
-            for(var optProp in options)
+            if( targetName == "options" )
             {
-               if(target.options.hasOwnProperty( optProp ))
+               continue;
+            }
+            var target = taskObj[targetName];
+            if( target )
+            {
+               for( var prop in taskObj.options.default )
                {
-                  target.options[optProp] = options[optProp];
+                  if( !target.hasOwnProperty( prop ) )
+                  {
+                     target[prop] = taskObj.options.default[prop];
+                  }
+                  /*
+                  else if(prop == "options")
+                  {
+                     var options = task.default[prop];
+                     for(var optProp in options)
+                     {
+                        if(target.options.hasOwnProperty( optProp ))
+                        {
+                           target.options[optProp] = options[optProp];
+                        }
+                     }
+                  }
+                  //*/
                }
             }
-         }*/
+         }
       }
    }
-
+   config.copyDefaults();
    grunt.initConfig( config );
-
-   grunt.registerTask( "default", ["ts:default"] );
-
-   grunt.registerMultiTask( "gen", function()
-   {
-      generateModuleRoots(
-         this.data.template,
-         this.data.root,
-         this.data.regex,
-         this.data.ext,
-         this.data.extArr
-      );
-   } );
-
-   grunt.registerMultiTask( "def", function()
-   {
-      var dts = require( 'dts-bundle' );
-      dts.bundle( this.data );
-      var path = require( "path" );
-      var file = path.resolve( this.data.main, "../", this.data.out );
-      _fs.writeFileSync( file, _fs.readFileSync( file ).toString().replace( /__nnet\//g, "" ), 'utf-8' );
-   } );
-
-   grunt.registerTask( "rjs", function()
-   {
-      var r = require( "requirejs" );
-
-      function requirejsOptimize(resultingConfig)
-      {
-         resultingConfig = resultingConfig || {};
-
-         for(var prop in config.requirejs.main)
-         {
-            if(!(resultingConfig.hasOwnProperty( prop )))
-            {
-               resultingConfig[prop] = config.requirejs.main[prop];
-            }
-         }
-
-         // now run the optimizer
-         r.optimize( resultingConfig, config.requirejs.out, config.requirejs.err );
-      }
-
-      config.requirejs.builds.forEach( requirejsOptimize );
-   } );
 
    //
    // Utility functions
