5a50a764eeed8cf3a562e3b0fb657040505e78ca
Malachi Griffie <malachi@nexussays.com>
2014-09-13 05:51:45 -0700

    Removed custom JSON parsing since all browsers now support it natively

--- nnet-js/src/nnet/serialization/JsonParser.ts
+++ nnet-js/src/nnet/serialization/JsonParser.ts
@@ -4,104 +4,15 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-///ts:import=type
-import type = require('../type'); ///ts:import:generated
-///ts:import=Types
-import Types = require('../Types'); ///ts:import:generated
-
-/**
- * Signature => public static function encode(object:Object):String
- * @private
- */
-export var encode: (object: any) => string;
-
-/**
- * Signature => public static function decode(json:String):Object
- * @private
- */
-export var decode: (json: string) => any;
-
-// determine if we can use the native JSON parser
-if(JSON && JSON.stringify && typeof JSON.stringify === "function")
+export = JsonParser;
+module JsonParser
 {
-   encode = JSON.stringify;
-   decode = JSON.parse;
-   console.log( "Using native JSON" );
-}
-else
-{
-   //TODO: This needs to not be doing eval, then again, it's only for IE < 8
-   encode = (value: string) =>
+   export var decode: (text: string, reviver?: (key: any, value: any) => any) => any = JSON.parse;
+
+   export function encode(value: any, replacer?: (key: string, value: any) => any, space?: any): string;
+   export function encode(value: any, replacer?: any[], space?: any):string;
+   export function encode(value: any, replacer?: any, space?: any): string
    {
-      try
-      {
-         return eval( "(" + value + ")" );
-      }
-      catch(ex)
-      {
-         ex.name = "JsonEncodeError";
-         throw ex;
-      }
-   };
-   decode = (value: any) =>
-   {
-      function recurse(x, isArray: boolean=false)
-      {
-         var ret = "",
-             result;
-         for(var property in x)
-         {
-            var val = x[property];
-            var t = type(val);
-            if(t != Types.function && isArray !== true)
-            {
-               ret += "\"" + property + "\":";
-            }
-
-            result = getValue( val, t );
-            ret += result ? result + "," : "";
-         }
-         //remove that last comma
-         return ret.replace( /,$/, "" );
-      }
-
-      function getValue(val, t: number): string
-      {
-         switch(t)
-         {
-            case Types.number:
-            case Types.boolean:
-            case Types.null:
-            case Types.undefined:
-               return val + "";
-            case Types.object:
-               return "{" + recurse( val ) + "}";
-            case Types.array:
-               return "[" + recurse( val, true ) + "]";
-            case Types.function:
-               return "";
-            case Types.date:
-               return "Date(" + val.getTime() + ")";
-            default:
-               val += "";
-               //escape any escape characters, then escape any quotes
-               return "\"" + val.replace( /\\/g, "\\\\" ).replace( /[\r\n]/g, "\\n" ).replace( /"/g, "\\\"" ) + "\"";
-         }
-      }
-
-      return getValue( value, type( value ) );
-   };
-   console.log( "Using NNet JSON" );
+      return JSON.stringify( value, replacer, space );
+   }
 }
-
-//interface Date
-//{
-//   toJSON: (string) => any;
-//}
-/**
- * Override the toJSON method for Date to return the milliseconds since epoch
- */
-//Date.prototype.toJSON = (key) =>
-//{
-//   return this.getTime();
-//}
