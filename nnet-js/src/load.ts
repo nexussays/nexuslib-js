8a174ff7dc9a05e0f5fc9e0914330526a85c5ec8
Malachi Griffie <malachi@nexussays.com>
2014-09-17 23:23:46 -0700

    Added copyright info to load.ts

--- nnet-js/src/load.ts
+++ nnet-js/src/load.ts
@@ -1,4 +1,10 @@
-﻿export = load;
+﻿// Copyright Malachi Griffie <malachi@nexussays.com>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+export = load;
 
 var remaining: { [dependency: string]: number } = {};
 var completionCallbacks: { [concatenatedDependencies: string]: Array<() => void> } = {};
