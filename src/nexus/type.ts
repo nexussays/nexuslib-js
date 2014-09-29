67375f0bd660c908ba0db4c0fc9a96032fa6c678
Malachi Griffie <malachi@nexussays.com>
2014-09-29 00:43:45 -0700

    Removed some unnecessary types from type()

--- src/nexus/type.ts
+++ src/nexus/type.ts
@@ -8,42 +8,37 @@ export = type;
 
 enum type
 {
-   // ReSharper disable InconsistentNaming
    "undefined",
-   "null",
    "object",
    "array",
-   // ReSharper disable once UsingOfReservedWord
    "boolean",
    "node",
-   "window",
    "date",
    "string",
    "function",
    "number",
    "regexp"
-   // ReSharper restore InconsistentNaming
 }
 
 module type
 {
    export function of(obj: any): type
    {
+      if(obj === null || obj === undefined)
+      {
+         return type.undefined;
+      }
       var objtype = typeof obj;
-      var result = type[objtype];
+      var result: type = type[objtype];
       if(objtype === "object")
       {
-         result = obj === null ? type.null :
-                     obj instanceof Array ? type.array :
-                        obj instanceof RegExp ? type.regexp :
-                           //this fails to capture the document node
-                           //obj instanceof HTMLElement ? "element" :
-                           "nodeType" in obj ? type.node :
-                              obj === window ? type.window :
-                                 obj instanceof Date ? type.date :
-                                    //highly unlikely that a string was created from the constructor, so it is last
-                                    obj instanceof String ? type.string :
-                                       result;
+         result = obj instanceof Array ? type.array :
+                     obj instanceof RegExp ? type.regexp :
+                        "nodeType" in obj ? type.node :
+                           obj instanceof Date ? type.date :
+                              //highly unlikely that a string was created from the constructor, but just in case
+                              obj instanceof String ? type.string :
+                                 result;
       }
       else if(objtype === "function" && obj instanceof RegExp)
       {
@@ -52,22 +47,3 @@ module type
       return result;
    }
 }
-
-/* alt
-var TYPES = {
-    'undefined'        : 'undefined',
-    'number'           : 'number',
-    'boolean'          : 'boolean',
-    'string'           : 'string',
-    '[object Function]': 'function',
-    '[object RegExp]'  : 'regexp',
-    '[object Array]'   : 'array',
-    '[object Date]'    : 'date',
-    '[object Error]'   : 'error'
-},
-TOSTRING = Object.prototype.toString;
-
-function type.of(o) {
-    return TYPES[typeof o] || TYPES[TOSTRING.call(o)] || (o ? 'object' : 'null');
-};
-*/
