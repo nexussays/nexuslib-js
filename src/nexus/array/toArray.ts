891aace3cf7d1a85e4e52a5e01f60edb6c354685
nexus <nexus@nexussays.com>
2018-02-08 02:22:32 -0800

    updated license

--- src/nexus/array/toArray.ts
+++ src/nexus/array/toArray.ts
@@ -1,4 +1,4 @@
-﻿// Copyright Malachi Griffie <malachi@nexussays.com>
+﻿// Copyright M Griffie <nexus@nexussays.com>
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
--- src/nexus/object/extendPrototype.ts
+++ src/nexus/object/extendPrototype.ts
@@ -1,4 +1,4 @@
-﻿// Copyright Malachi Griffie <malachi@nexussays.com>
+﻿// Copyright M Griffie <nexus@nexussays.com>
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -13,3 +13,12 @@ function extendPrototype(derived: any, parents: any[])
       Object.getOwnPropertyNames( parent.prototype ).forEach( name => derived.prototype[name] = parent.prototype[name] );
    } );
 }
+
+//function inheritPrototype(sub:any, super:any)
+//{
+//   var foo = function(){};
+//   foo.prototype = super.prototype;
+//   var superCopy = new foo();
+//   superCopy.constructor = sub;
+//   sub.prototype = superCopy;
+//}
